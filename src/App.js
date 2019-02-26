e86a59f6b8d7d2f501857ada95ee0add03b2e753
n18016 <n18016@std.it-college.ac.jp>
2019-02-26 01:47:49 +0000

    react

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react'
+import './App.css'
+import jsonp from 'fetch-jsonp'
+
+export default class App extends Component {
+    state = {'key':'0'}
+    loadedJSON = (res) => this.setState(res)
+    getHoroscope = (date) => {
+        const URI = 'http://api.jugemkey.jp/api/horoscope/free/jsonp/'
+
+        jsonp(URI + date)
+            .then(res => res.json())
+            .then(this.loadedJSON)
+
+    }
+    componentWillMount () {
+        this.getHoroscope('')
+    }
+    render() {
+        console.log(this.state)
+        if (!this.state.horoscope) return <div className='App' />
+        return (
+            <div className="App">
+                <NormalView data={this.state.horoscope['2019/02/27'][this.state.key]} />
+                <select onChange={e => this.doChange(e)}>
+            <option value="0" key="0">牡羊座</option>
+            <option value="1" key="1">牡牛座</option>
+            <option value="2" key="2">双子座</option>
+            <option value="3" key="3">蟹座</option>
+            <option value="4" key="4">獅子座</option>
+            <option value="5" key="5">乙女座</option>
+            <option value="6" key="6">天秤座</option>
+            <option value="7" key="7">さそり座</option>
+            <option value="8" key="8">射手座</option>
+            <option value="9" key="9">山羊座</option>
+            <option value="10" key="10">水瓶座</option>
+            <option value="11" key="11">魚座</option>
+        </select>
+            </div>
+        )
+    }
+    doChange (e) {
+        this.setState({key: e.target.value})
+    }
+}
+
+const NormalView = props => {
+    let strdata = JSON.stringify(props.data)
+
+    return (
+        <div>
+            <p>出来事：{props.data.content}</p>
+            <p>ラッキーアイテム：{props.data.item}</p>
+            <p>ラッキーカラー：{props.data.color}</p>
+            <p>金運：{props.data.money}</p>
+            <p>仕事運：{props.data.job}</p>
+            <p>恋愛運：{props.data.love}</p>
+            <p>ランキング：今日のあなたの順位は{props.data.rank}位</p>
+        </div>
+    )
+}
